d95fc82f3de76945eb46f9d086b1783846bf8dcd
kalamchi <karenaikalam@gmail.com>
2018-04-24 15:29:41 +0800

    .

--- connect.php
+++ connect.php
@@ -1,22 +1,21 @@
 <?php 
-session_start(); 
-?>
 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        
+	$servername = "localhost"; //伺服器名稱
+	$username = "root"; //使用者名稱(用root即可)
+	$pass = ""; //密碼(如果沒有更改，則用空字串即可)
+	$dbname = "scphoto"; //資料庫名稱
 
-<?php
-$db_server = "localhost";
+	// 建立連線
+	$link = mysqli_connect($servername,$username, $pass, $dbname);
 
-$db_name = "scphoto";
+	// 確認連練
+	if (!$link) {  //連線失敗，則顯示錯誤訊息
+    	die("Connection failed: " . mysqli_connect_error());
+	}
+	//echo "Connected successfully"; //連線成功，則印出此行
 
-$db_user = "root";
-
-$db_passwd = "";
-
-if(!@mysql_connect($db_server, $db_user, $db_passwd))
-        die("無法對資料庫連線");
-
-
-mysql_query("SET NAMES utf8");
-
+	//設置mysql執行編碼為utf-8
+	mysqli_query($link,"set names utf8"); 
+	
 ?>
